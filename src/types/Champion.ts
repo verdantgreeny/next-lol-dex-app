0370840a8f2fdc3e6a24461a9d77b7d2f37a83a3
verdantgreeny <verdantgreeny@gmail.com>
2025-03-11 20:10:50 +0900

    ✨ update: champion detail 타입 정의

--- src/types/Champion.ts
+++ src/types/Champion.ts
@@ -6,3 +6,43 @@ export type Champion = {
     full: string;
   };
 };
+
+export type ChampionDetail = {
+  id: string;
+  name: string;
+  title: string;
+  lore: string;
+  partype: string;
+  tags: string[];
+  stats: {
+    hp: number;
+    hpperlevel: number;
+    armor: number;
+    attackdamage: number;
+    movespeed: number;
+    [key: string]: number;
+  };
+  spells: {
+    id: string;
+    name: string;
+    description: string;
+    tooltip: string;
+    image: {
+      full: string;
+    };
+  }[];
+  passive: {
+    name: string;
+    description: string;
+    image: {
+      full: string;
+    };
+  };
+  allytips: string[];
+  enemytips: string[];
+  skins: {
+    id: string;
+    name: string;
+    num: number;
+  }[];
+};
