254b64447f5d2e486fe10fa115e8904c5eb72477
verdantgreeny <verdantgreeny@gmail.com>
2025-03-11 17:03:33 +0900

    ✨ update: 챔피언 목록 가져오는 기능 추가

--- src/utils/serverApi.ts
+++ src/utils/serverApi.ts
@@ -1,4 +1,5 @@
 import { API_BASE_URL } from "@/constant/riotConstants";
+import { Champion } from "@/types/Champion";
 import { Item } from "@/types/Items";
 
 export const fetchItemList = async (): Promise<Item[]> => {
@@ -7,5 +8,15 @@ export const fetchItemList = async (): Promise<Item[]> => {
   });
   const data = await res.json();
   //   console.log(data);
+  return Object.values(data.data);
+};
+
+export const fetchChampionList = async (): Promise<Champion[]> => {
+  const res = await fetch(
+    `${API_BASE_URL}/champion.json`,
+    { next: { revalidate: 60 * 60 * 24 } } //24시간마다
+  );
+  const data = await res.json();
+
   return Object.values(data.data);
 };
