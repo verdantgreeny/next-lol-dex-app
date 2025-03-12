09158db0cff135092d790b1e6799de45cb5a0da9
verdantgreeny <verdantgreeny@gmail.com>
2025-03-12 20:29:00 +0900

    ♻️ refactor: ChampionCard 컴포넌트 분리

--- src/app/champions/page.tsx
+++ src/app/champions/page.tsx
@@ -1,7 +1,5 @@
-import { IMAGE_BASE_URL } from "@/constants/riotConstants";
+import ChampionCard from "@/components/ChampionCard";
 import { fetchChampionList } from "@/utils/serverApi";
-import Image from "next/image";
-import Link from "next/link";
 import React from "react";
 
 const ChampionsList = async () => {
@@ -12,22 +10,7 @@ const ChampionsList = async () => {
       <h1 className="text-3xl font-bold mb-6">챔피언 목록</h1>
       <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 w-full">
         {champions.map((champion) => (
-          <Link key={champion.id} href={`/champions/${champion.id}`}>
-            <article className="bg-white p-4 rounded-lg shadow-md hover:shadow-lg transition-shadow flex flex-col items-center">
-              <Image
-                src={`${IMAGE_BASE_URL}/champion/${champion.image.full}`}
-                alt={champion.name}
-                width={96}
-                height={96}
-              />
-              <h2 className="text-center font-medium mt-2 text-red-800">
-                {champion.name}
-              </h2>
-              <p className="text-gray-800 text-sm text-center truncate w-full">
-                {champion.title}
-              </p>
-            </article>
-          </Link>
+          <ChampionCard key={champion.id} champion={champion} />
         ))}
       </div>
     </div>
