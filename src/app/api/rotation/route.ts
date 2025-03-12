310a04c9ef47f96b30e0943c1208d77f48d775fd
verdantgreeny <verdantgreeny@gmail.com>
2025-03-12 21:48:19 +0900

    ♻️ refactor: champion-ratation-url 상수처리

--- src/app/api/rotation/route.ts
+++ src/app/api/rotation/route.ts
@@ -1,15 +1,14 @@
+import { ROTATIONS_URL } from "@/constants/riotConstants";
+
 export async function GET() {
   const apiKey = process.env.RIOT_API_KEY;
   if (!apiKey) return Response.json({ error: "API key 없음" }, { status: 500 });
 
-  const res = await fetch(
-    "https://kr.api.riotgames.com/lol/platform/v3/champion-rotations",
-    {
-      headers: {
-        "X-Riot-Token": apiKey,
-      },
-    }
-  );
+  const res = await fetch(ROTATIONS_URL, {
+    headers: {
+      "X-Riot-Token": apiKey,
+    },
+  });
 
   const { freeChampionIds } = await res.json();
   //   console.log(res2);
