922c1fe18b26a16f95500ad0a936e4a2171d3fa8
verdantgreeny <verdantgreeny@gmail.com>
2025-03-16 23:45:35 +0900

    ✨ update: 아이템 페이지 반응형으로 개선

--- src/app/items/[name]/page.tsx
+++ src/app/items/[name]/page.tsx
@@ -19,18 +19,18 @@ const ItemDetail = async ({ params }: { params: { name: string } }) => {
   }
 
   return (
-    <div className="max-w-5xl mx-auto p-4 space-y-8">
-      <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold mb-6 text-[var(--gold)] ">
+    <div className="max-w-5xl mx-auto py-8 space-y-8">
+      <h1 className="text-3xl font-bold mb-6 text-[var(--gold)] ">
         {itemName}
       </h1>
 
-      <div className="bg-[var(--magic-engineering-black)] p-4 rounded-lg flex flex-col items-center justify-center gap-4 border-2 border-[var(--gray-1)] max-w-2xl mx-auto">
+      <div className="bg-[var(--magic-engineering-black)] p-4 rounded-lg flex flex-col items-center justify-center gap-4 border-2 border-[var(--gray-1)] max-w-lg mx-auto">
         <Image
           src={`${IMAGE_BASE_URL}/item/${itemData.image.full}`}
           alt={itemData.name}
-          width={150}
-          height={150}
-          className="w-24 h-24 md:w-32 md:h-32 lg:w-40 lg:h-40"
+          width={124}
+          height={124}
+
         />
 
         <div className="flex flex-col md:flex-row gap-2 md:gap-4">
@@ -49,7 +49,7 @@ const ItemDetail = async ({ params }: { params: { name: string } }) => {
           {itemData.tags.map((tag, index) => (
             <span
               key={index}
-              className="px-2 py-1 bg-[var(--gray-1)] text-[var(--magic-engineering-black)] rounded text-xs md:text-sm"
+              className="px-2 py-1 bg-[var(--gray-1)] text-[var(--magic-engineering-black)] rounded text-sm"
             >
               {tag}
             </span>
@@ -58,10 +58,8 @@ const ItemDetail = async ({ params }: { params: { name: string } }) => {
       </div>
 
       <div className="mt-6 w-full">
-        <h2 className="text-lg md:text-xl lg:text-2xl font-bold mb-2 text-[var(--gold)]">
-          설명
-        </h2>
-        <p className="text-xs md:text-sm text-justify text-[var(--gray-1)] mt-2">
+        <h2 className="text-xl font-bold mb-2 text-[var(--gold)]">설명</h2>
+        <p className="text-sm text-justify text-[var(--gray-1)] mt-2">
           {itemData.plaintext}
         </p>
       </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       >
         <Providers>
           <Header />
-          <main className="bg-[var(--gray-3)] min-h-full flex-1 px-20">{children}</main>
+          <main className="bg-[var(--gray-3)] min-h-full flex-1 px-4 md:px-20">{children}</main>
           <Footer />
         </Providers>
       </body>
