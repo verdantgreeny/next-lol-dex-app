331dce4aa3dd68db3457be1cb0bd9c5eef6a1030
verdantgreeny <verdantgreeny@gmail.com>
2025-03-17 01:39:38 +0900

    ♻️ refactor: url 환경변수 처리

--- src/constants/riotConstants.ts
+++ src/constants/riotConstants.ts
@@ -1,6 +1,3 @@
-export const API_BASE_URL =
-  "https://ddragon.leagueoflegends.com/cdn/15.5.1/data/ko_KR";
-export const IMAGE_BASE_URL =
-  "https://ddragon.leagueoflegends.com/cdn/15.5.1/img";
-export const ROTATIONS_URL =
-  "https://kr.api.riotgames.com/lol/platform/v3/champion-rotations";
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+export const IMAGE_BASE_URL = process.env.NEXT_PUBLIC_IMAGE_BASE_URL;
+export const ROTATIONS_URL = process.env.NEXT_PUBLIC_ROTATIONS_URL;
