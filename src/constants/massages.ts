e6c6a38227a62641cde15d62bb600e2c2e00f56f
verdantgreeny <verdantgreeny@gmail.com>
2025-03-17 15:39:58 +0900

    ♻️ refactor: 에러 미시지 상수처리

--- src/constants/massages.ts
+++ src/constants/massages.ts
@@ -1,1 +1,8 @@
 export const ITEM_NOT_FOUND_MESSAGE = "아이템을 찾을 수 없습니다.";
+
+export const ERRORS = {
+  NO_API_KEY: { error: "API key 없음" },
+  NO_URL: { error: "ROTATIONS_URL 없음" },
+  ROTATION_FETCH_FAIL: { error: "free champion rotation 가져오기 실패" },
+  FETCH_FAIL: "데이터를 불러오는 중 오류가 발생했습니다.",
+};
--- src/constants/riotConstants.ts
+++ src/constants/riotConstants.ts
@@ -1,3 +1,7 @@
 export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 export const IMAGE_BASE_URL = process.env.NEXT_PUBLIC_IMAGE_BASE_URL;
 export const ROTATIONS_URL = process.env.NEXT_PUBLIC_ROTATIONS_URL;
+
+export const CACHE_TIME = {
+  REVALIDATE_24H: 60 * 60 * 24, // 24시간
+};
