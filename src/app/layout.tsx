a5b0cc180a6c3a0a96e8c33f958100ce2f7cd354
verdantgreeny <verdantgreeny@gmail.com>
2025-03-18 16:03:57 +0900

    🎉 add: favicon추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ import ThemeToggle from "@/components/layout/ThemeToggle";
 export const metadata: Metadata = {
   title: "리그 오브 레전드 정보 앱",
   description: "Riot Games API를 활용하여 챔피언과 아이템 정보를 제공합니다.",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
--- src/utils/riotApi.ts
+++ src/utils/riotApi.ts
@@ -3,11 +3,13 @@ import { fetchChampionList } from "./serverApi";
 
 export const getChampionRotation = async (): Promise<Champion[]> => {
   try {
+    // console.time("병렬 처리");
     const [rotationRes, champions] = await Promise.all([
       fetch("/api/rotation"),
       fetchChampionList(),
     ]);
     const freeChampionIds = await rotationRes.json();
+    // console.timeEnd("병렬 처리");
     // console.log("로테이션 데이타:", freeChampionIds);
     return freeChampionIds.map((id: number) => {
       const numId = id.toString();
