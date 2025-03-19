edf163e02862ec8bc1aaea193d55974f77328422
verdantgreeny <verdantgreeny@gmail.com>
2025-03-19 09:44:02 +0900

    🩹 fix: tailwind config 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko" suppressHydrationWarning>
+    <html lang="ko">
       <body suppressHydrationWarning className="flex flex-col min-h-screen ">
         <Providers>
           <Header />
